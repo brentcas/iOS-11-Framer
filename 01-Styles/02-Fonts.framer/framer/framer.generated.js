8e988a3e63b4d79f804655ea10dda4c96a8b6b2d
brentcas <caswell.brent@gmail.com>
2017-12-20 22:02:28 -0600

    Opened in latest version of Framer

--- 01-Styles/02-Fonts.framer/framer/framer.generated.js
+++ 01-Styles/02-Fonts.framer/framer/framer.generated.js
@@ -7,7 +7,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":1,"select
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":1,"selectedHand":"","deviceType":"fullscreen","contentScale":1,"hideBezel":true,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"02. Fonts.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"02-Fonts.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
